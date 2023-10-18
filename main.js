c2c351159fa7b4d5e9dc8165b00edc2c9afebbf0
Todor Mladenov <toshko958@gmail.com>
2023-10-18 16:02:12 +0200

    Added the movement of the wizard

--- main.js
+++ main.js
@@ -7,23 +7,52 @@ gameStartElement.addEventListener('click', startGame);
 document.addEventListener('keydown', onKeyDown);
 document.addEventListener('keyup', onKeyUp);
 
+const keys = {};
+const player = {
+    x: 150,
+    y: 100,
+};
+const gameSettings = {
+    movementSpeed: 2,
+    wizardHight: 100,
+    wizardWight: 82,
+};
+
 function startGame(e) {
     gameStartElement.classList.add('hide');
 
     let wizard = document.createElement('div');
     wizard.classList.add('wizard');
-    wizard.style.top = '200px';
-    wizard.style.left = '200px';
+    wizard.style.top = player.y + 'px';
+    wizard.style.left = player.x + 'px';
     gameAreaElement.appendChild(wizard);
     window.requestAnimationFrame(game);
 }
 
 function game() {
-    console.log(keys);
+    let wizard = document.querySelector('.wizard');
+
+    if (keys.ArrowUp) {
+        player.y = Math.max(player.y - gameSettings.movementSpeed, 0);
+    }
+
+    if (keys.ArrowDown) {
+        player.y = Math.min(player.y + gameSettings.movementSpeed, (gameAreaElement.offsetHeight - gameSettings.wizardHight));
+    }
+
+    if (keys.ArrowLeft) {
+        player.x = Math.max(player.x - gameSettings.movementSpeed, 0);
+    }
+
+    if (keys.ArrowRight) {
+        player.x = Math.min(player.x + gameSettings.movementSpeed, (gameAreaElement.offsetWidth - gameSettings.wizardWight));
+    }
+
+    wizard.style.top = player.y + 'px';
+    wizard.style.left = player.x + 'px';
+
     window.requestAnimationFrame(game);
 }
-
-let keys = {};
 
 function onKeyDown(e) {
     keys[e.code] = true;
