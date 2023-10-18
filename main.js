5238ef8ff84363b18561173d37aeea1dfc2a9964
Todor Mladenov <toshko958@gmail.com>
2023-10-18 14:21:37 +0200

    Added Event listener for keyup and keydown

--- main.js
+++ main.js
@@ -4,11 +4,31 @@ const gameOverElement = document.querySelector('.game-over');
 const gameScoreElement = document.querySelector('.game-score');
 
 gameStartElement.addEventListener('click', startGame);
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
 
-function startGame(e){
+function startGame(e) {
     gameStartElement.classList.add('hide');
 
     let wizard = document.createElement('div');
     wizard.classList.add('wizard');
+    wizard.style.top = '200px';
+    wizard.style.left = '200px';
     gameAreaElement.appendChild(wizard);
+    window.requestAnimationFrame(game);
+}
+
+function game() {
+    console.log(keys);
+    window.requestAnimationFrame(game);
+}
+
+let keys = {};
+
+function onKeyDown(e) {
+    keys[e.code] = true;
+}
+
+function onKeyUp(e) {
+    keys[e.code] = false;
 }
